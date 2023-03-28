2a2ef168f3a11fba16d1f27753dbd83d4343e2e9
donald <dmaisiba18@gmail.com>
2023-03-28 06:39:52 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print a string to stdiut
+ * @str: string
+ * Return: lenght of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
